17bc186b0f75a77f9acfbc93a3b1ed0b15f4f8ff
CVanderbilt <eduhgb5198@gmail.com>
2021-07-25 12:15:27 +0200

    line deletion moved to own file, previously on kt_functions

--- extras/noncanon_gnl/kt_functions.c
+++ extras/noncanon_gnl/kt_functions.c
@@ -55,28 +55,6 @@ int	get_key(t_key *key)
 	return (1);
 }
 
-void	line_deletion(t_key *k)
-{
-	int	eol1;
-	int	offset;
-	int	c;
-	int	r;
-	int	i;
-
-	i = -1;
-	if (!cursor_position(0, &r, &c))
-		return ;
-	offset = k->l.last_offset;
-	if (k->l.last_key == KT_UNRECOGNIZED || k->l.last_key == KT_EOL)
-		offset = get_offset(k, c);
-	eol1 = k->w.ws_col - offset - 1;
-	goto_cursor(k, 0);
-	cursor_position(0, &r, &c);
-	while (++i < (int)k->l.cursor_max)
-		write(0, " ", 1);
-	tputs(tgoto(tgetstr("cm", NULL), c, r), 0, ft_putchar0);
-}
-
 int	ft_save(t_key *key, const char *str)
 {
 	if (key->h.hist[key->h.pos])
