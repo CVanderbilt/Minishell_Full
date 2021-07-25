438d535e0aa08a1107611c398cd5016157af122e
CVanderbilt <eduhgb5198@gmail.com>
2021-07-25 12:03:46 +0200

    redirections1 red_open_dup splitted

--- src/redirections1.c
+++ src/redirections1.c
@@ -161,6 +161,29 @@ int	red_here_doc(t_shell *s, const char *file, int fd)
 	return (0);
 }
 
+int	red_out(t_rod *r, const char *file, t_redir type)
+{
+	if (type == RED_OUT)
+		r->fd2 = open(file, O_WRONLY | O_CREAT | O_TRUNC, S_IRWXU);
+	else
+		r->fd2 = open(file, O_WRONLY | O_CREAT | O_APPEND, S_IRWXU);
+	if (fstat(r->fd2, &r->st))
+		return (-1);
+	return (1);
+}
+
+
+int	red_open_dup_aux(t_shell *s, int fd, t_redir type)
+{
+	if (fd < 0 && (type == RED_HERE_DOC || type == RED_IN))
+		fd = 0;
+	else if (fd < 0)
+		fd = 1;
+	if (fd == s->stderr_save || fd == s->stdin_save || fd == s->stdout_save)
+		return (0);
+	return (1);
+}
+
 /*
 *	red_open_dup depending on the redir type duplicates the fd corresponding
 *	to the redirecting/ed file in the selected fd, in the < and << redirections
@@ -172,19 +195,11 @@ int	red_open_dup(t_shell *s, int fd, const char *file, t_redir type)
 {
 	t_rod	r;
 
-	if (fd < 0 && (type == RED_HERE_DOC || type == RED_IN))
-		fd = 0;
-	else if (fd < 0)
-		fd = 1;
-	if (fd == s->stderr_save || fd == s->stdin_save || fd == s->stdout_save)
+	if (!red_open_dup_aux(s, fd, type))
 		return (0);
 	if (type == RED_OUT || type == RED_OUT_APP)
 	{
-		if (type == RED_OUT)
-			r.fd2 = open(file, O_WRONLY | O_CREAT | O_TRUNC, S_IRWXU);
-		else
-			r.fd2 = open(file, O_WRONLY | O_CREAT | O_APPEND, S_IRWXU);
-		if (fstat(r.fd2, &r.st))
+		if (red_out(&r, file, type) == -1)
 			return (-1);
 	}
 	else if (type == RED_IN)
@@ -198,7 +213,7 @@ int	red_open_dup(t_shell *s, int fd, const char *file, t_redir type)
 	else
 		return (-1);
 	dup2(r.fd2, fd);
-	if (r.fd2 > 2)
+	if ((dup2(r.fd2, fd) || 1) && r.fd2 > 2)
 		ft_close(r.fd2);
 	return (1);
 }
