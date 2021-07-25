4ed7b9d842ef6c5c3f8f4589d926485944d52984
CVanderbilt <eduhgb5198@gmail.com>
2021-07-25 12:20:31 +0200

    kt print splitted

--- extras/noncanon_gnl/kt_print.c
+++ extras/noncanon_gnl/kt_print.c
@@ -2,26 +2,10 @@
 #include "utils.h"
 #include "motion.h"
 
-int	kf_print(t_key *k)
+static void	print_last(t_key *k, char *save, int row, int col)
 {
-	char	*save;
-	char	buff[2];
+	unsigned int	resto;
 
-	tputs(tgetstr("sc", NULL), 0, &ft_putchar0);
-	save = ft_strdup(k->l.str + k->l.cursor);
-	if (!save)
-		return (1);
-	int	row;
-	int	col;
-	cursor_position(0, &row, &col);
-	buff[1] = 0;
-	buff[0] = k->key[0];
-	write(0, buff, 1);
-	ft_putstr_fd(0, save);
-	tputs(tgetstr("rc", NULL), 0, &ft_putchar0);
-
-	unsigned int resto;
-	
 	resto = k->w.ws_col - col;
 	if (resto <= ft_strlen(save))
 	{
@@ -29,9 +13,27 @@ int	kf_print(t_key *k)
 		if (row + col / k->w.ws_col >= k->w.ws_row && col % k->w.ws_col == 1)
 			tputs(tgetstr("up", NULL), 0, &ft_putchar0);
 	}
-
-
 	free (save);
+}
+
+int	kf_print(t_key *k)
+{
+	char	*save;
+	char	buff[2];
+	int		row;
+	int		col;
+
+	tputs(tgetstr("sc", NULL), 0, &ft_putchar0);
+	save = ft_strdup(k->l.str + k->l.cursor);
+	if (!save)
+		return (1);
+	cursor_position(0, &row, &col);
+	buff[1] = 0;
+	buff[0] = k->key[0];
+	write(0, buff, 1);
+	ft_putstr_fd(0, save);
+	tputs(tgetstr("rc", NULL), 0, &ft_putchar0);
+	print_last(k, save, row, col);
 	k->l.write(&k->l, buff);
 	move_cursor_right(k);
 	return (1);
